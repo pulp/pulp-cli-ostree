pulp/pulp-cli

commit aa50bc63e949e7f413bce6bc10d323d119e61b09

Fix content group with polymorphic options
fixes: #492

pulpcore/cli/ansible/repository.py
@@ -34,7 +34,6 @@ from pulpcore.cli.common.generic import (
     resource_option,
     retained_versions_option,
     show_command,
-    type_option,
     update_command,
     version_command,
 )
@@ -131,13 +130,6 @@ content_options = [
         expose_value=False,
         cls=GroupOption,
     ),
-    type_option(
-        choices={
-            "collection-version": PulpAnsibleCollectionVersionContext,
-            "role": PulpAnsibleRoleContext,
-        },
-        default="collection-version",
-    ),
     href_option,
 ]
 content_json_callback = create_content_json_callback(schema=CONTENT_LIST_SCHEMA)
@@ -159,13 +151,6 @@ modify_options = [
     Each object must contain the following keys: "name", "namespace", "version".
     The argument prefixed with the '@' can be the path to a JSON file with a list of objects."""
         ),
-    ),
-    type_option(
-        choices={
-            "collection-version": PulpAnsibleCollectionVersionContext,
-            "role": PulpAnsibleRoleContext,
-        },
-        default="collection-version",
     ),
 ]
 

pulpcore/cli/common/generic.py
@@ -986,8 +986,6 @@ def repository_content_command(**kwargs: Any) -> click.Group:
     """A factory that creates a repository content command group."""
 
     content_contexts = kwargs.pop("contexts", {})
-    names = list(content_contexts.keys()) + ["all"]
-    content_contexts.update({"all": PulpContentContext})
 
     def version_callback(
         ctx: click.Context, param: click.Parameter, value: Optional[int]
@@ -1002,29 +1000,30 @@ def repository_content_command(**kwargs: Any) -> click.Group:
         )
         return repo_ver_ctx
 
-    @click.command("list")
-    @click.option("-t", "--type", "type", type=click.Choice(names), default=names[0])
+    @pulp_command("list")
+    @click.option("--all-types", is_flag=True)
     @limit_option
     @offset_option
     @repository_option
     @click.option("--version", type=int, callback=version_callback)
     @pass_pulp_context
+    @pass_content_context
     def content_list(
+        content_ctx: PulpContentContext,
         pulp_ctx: PulpContext,
         version: PulpRepositoryVersionContext,
-        offset: Optional[int],
-        limit: Optional[int],
-        type: Optional[str],
+        offset: int,
+        limit: int,
+        all_types: bool,
         **params: Any,
     ) -> None:
         parameters = {k: v for k, v in params.items() if v is not None}
         parameters.update({"repository_version": version.pulp_href})
-        result = content_contexts[type](pulp_ctx).list(
-            limit=limit, offset=offset, parameters=parameters
-        )
+        content_ctx = PulpContentContext(pulp_ctx) if all_types else content_ctx
+        result = content_ctx.list(limit=limit, offset=offset, parameters=parameters)
         pulp_ctx.output_result(result)
 
-    @click.command("add")
+    @pulp_command("add")
     @repository_option
     @click.option("--base-version", type=int, callback=version_callback)
     @pass_content_context
@@ -1039,7 +1038,7 @@ def repository_content_command(**kwargs: Any) -> click.Group:
             base_version=base_version.pulp_href,
         )
 
-    @click.command("remove")
+    @pulp_command("remove")
     @click.option("--all", is_flag=True, help=_("Remove all content from repository version"))
     @repository_option
     @click.option("--base-version", type=int, callback=version_callback)
@@ -1055,7 +1054,7 @@ def repository_content_command(**kwargs: Any) -> click.Group:
             repo_ctx.pulp_href, remove_content=remove_content, base_version=base_version.pulp_href
         )
 
-    @click.command("modify")
+    @pulp_command("modify")
     @repository_option
     @click.option("--base-version", type=int, callback=version_callback)
     def content_modify(
@@ -1077,11 +1076,12 @@ def repository_content_command(**kwargs: Any) -> click.Group:
     if not kwargs.get("name"):
         kwargs["name"] = "content"
 
-    @click.group(**kwargs)
+    @pulp_group(**kwargs)
+    @type_option(choices=content_contexts)
     @pass_pulp_context
     @click.pass_context
     def content_group(ctx: click.Context, pulp_ctx: PulpContext) -> None:
-        ctx.obj = PulpContentContext(pulp_ctx)
+        pass
 
     for command, options in command_decorators.items():
         if options is not None:

tests/scripts/pulp_ansible/test_content.sh
@@ -54,13 +54,13 @@ expect_succ pulp ansible repository create --name "cli_test_ansible_repository"
 expect_succ pulp ansible repository content add --repository "cli_test_ansible_repository" --name "posix" --namespace "ansible" --version "1.3.0"
 expect_succ pulp ansible repository content list --repository "cli_test_ansible_repository" --version 1
 test "$(echo "$OUTPUT" | jq -r length)" -eq "1"
-expect_succ pulp ansible repository content add --repository "cli_test_ansible_repository" --type "role" --name "kubernetes-modules" --namespace "ansible" --version "0.0.1"
-expect_succ pulp ansible repository content list --repository "cli_test_ansible_repository" --version 2 --type "role"
+expect_succ pulp ansible repository content --type "role" add --repository "cli_test_ansible_repository" --name "kubernetes-modules" --namespace "ansible" --version "0.0.1"
+expect_succ pulp ansible repository content --type "role" list --repository "cli_test_ansible_repository" --version 2
 test "$(echo "$OUTPUT" | jq -r length)" -eq "1"
 
 if pulp debug has-plugin --name "core" --min-version "3.11.0"
 then
-  expect_succ pulp ansible repository content list --repository "cli_test_ansible_repository" --version 2 --type "all"
+  expect_succ pulp ansible repository content list --repository "cli_test_ansible_repository" --version 2 --all-types
   test "$(echo "$OUTPUT" | jq -r length)" -eq "2"
 fi
 

tests/scripts/pulp_file/test_content.sh
@@ -42,7 +42,7 @@ expect_succ pulp file repository content add --repository "cli_test_file_reposit
 expect_succ pulp file repository content add --repository "cli_test_file_repository" --sha256 "$sha256" --relative-path upload_test/test.txt --base-version 0
 expect_succ pulp file repository content list --repository "cli_test_file_repository" --version 1
 test "$(echo "$OUTPUT" | jq -r length)" -eq "1"
-expect_succ pulp file repository content list --repository "cli_test_file_repository" --type "all"
+expect_succ pulp file repository content list --repository "cli_test_file_repository" --all-types
 
 expect_succ pulp file repository content remove --repository "cli_test_file_repository" --sha256 "$sha256" --relative-path upload_test/test.txt
 expect_succ pulp file repository content list --repository "cli_test_file_repository"

pulp/pulp-cli-ostree

commit 6494f07d7e60c9e855c4635bb17427108823a795

Update run_container.sh
In this commit, recent failures caused by the commit https://github.com/pulp/pulp-cli/commit/aa50bc63e949e7f413bce6bc10d323d119e61b09 were addressed as well.

[noissue]

.ci/run_container.sh
@@ -35,10 +35,11 @@ FROM_TAG="${FROM_TAG:-latest}"
 
 if [ "${CONTAINER_FILE:+x}" ]
 then
+  IMAGE_TAG="ephemeral-build"
   "${CONTAINER_RUNTIME}" build --file "${BASEPATH}/assets/${CONTAINER_FILE}" --build-arg FROM_TAG="${FROM_TAG}" --tag ghcr.io/pulp/pulp:"${IMAGE_TAG}" .
 fi
 
-"${CONTAINER_RUNTIME}" run ${RM:+--rm} --detach --name "pulp-ephemeral" --volume "${BASEPATH}/settings:/etc/pulp" --publish "8080:80" "ghcr.io/pulp/pulp:${IMAGE_TAG}"
+"${CONTAINER_RUNTIME}" run ${RM:+--rm} --env S6_KEEP_ENV=1 ${PULP_API_ROOT:+--env PULP_API_ROOT} --detach --name "pulp-ephemeral" --volume "${BASEPATH}/settings:/etc/pulp" --publish "8080:80" "ghcr.io/pulp/pulp:${IMAGE_TAG}"
 
 # shellcheck disable=SC2064
 trap "${CONTAINER_RUNTIME} stop pulp-ephemeral" EXIT
@@ -58,7 +59,7 @@ do
   fi
 
   sleep 3
-  if curl --fail http://localhost:8080/pulp/api/v3/status/ > /dev/null 2>&1
+  if curl --fail "http://localhost:8080${PULP_API_ROOT:-/pulp/}api/v3/status/" > /dev/null 2>&1
   then
     echo "SUCCESS."
     break
@@ -67,7 +68,7 @@ do
 done
 
 # show pulpcore/plugin versions we're using
-curl -s http://localhost:8080/pulp/api/v3/status/ | jq '.versions|map({key: .component, value: .version})|from_entries'
+curl -s "http://localhost:8080${PULP_API_ROOT:-/pulp/}api/v3/status/" | jq '.versions|map({key: .component, value: .version})|from_entries'
 
 # Set admin password
 "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" pulpcore-manager reset-admin-password --password password

.github/workflows/main.yml
@@ -52,6 +52,11 @@ jobs:
             image_tag: "nightly"
             from_tag: "latest"
             container_file: "Containerfile.source"
+          - python: "3.7"
+            pulp_cli: "0.14.0"
+            image_tag: "latest"
+            from_tag: "latest"
+            container_file: "Containerfile"
           - python: "3.6"
             pulp_cli: "0.13.0"
             image_tag: "latest"

.github/workflows/nightly.yml
@@ -24,6 +24,11 @@ jobs:
             image_tag: "nightly"
             from_tag: "latest"
             container_file: "Containerfile.source"
+          - python: "3.7"
+            pulp_cli: "0.14.0"
+            image_tag: "latest"
+            from_tag: "latest"
+            container_file: "Containerfile"
           - python: "3.6"
             pulp_cli: "0.13.0"
             image_tag: "latest"

pulpcore/cli/ostree/repository.py
@@ -119,37 +119,27 @@ ref_options = [
         "--checksum", cls=GroupOption, expose_value=False, group=["name"], callback=ref_callback
     ),
 ]
-ref_list_options = [click.option("-t", "--type", "type", type=click.Choice(["ref"]), default="ref")]
 ref_content_command = repository_content_command(
     name="ref",
     contexts={"ref": PulpOstreeRefContentContext},
     add_decorators=ref_options,
     remove_decorators=ref_options,
-    list_decorators=ref_list_options,
 )
 
 commit_options = [click.option("--checksum", expose_value=False, callback=commit_callback)]
-commit_list_options = [
-    click.option("-t", "--type", "type", type=click.Choice(["commit"]), default="commit")
-]
 commit_content_command = repository_content_command(
     name="commit",
     contexts={"commit": PulpOstreeCommitContentContext},
     add_decorators=commit_options,
     remove_decorators=commit_options,
-    list_decorators=commit_list_options,
 )
 
 config_options = [click.option("--pulp_href", expose_value=False, callback=config_callback)]
-config_list_options = [
-    click.option("-t", "--type", "type", type=click.Choice(["config"]), default="config")
-]
 config_content_command = repository_content_command(
     name="config",
     contexts={"config": PulpOstreeConfigContentContext},
     add_decorators=config_options,
     remove_decorators=config_options,
-    list_decorators=config_list_options,
 )
 
 general_list_content_command = repository_content_command(

pyproject.toml
@@ -16,7 +16,8 @@ line_length = 100
 
 [tool.pytest.ini_options]
 markers = [
-  "script: Marks tests provided as shell scripts",
+  "script: tests provided as shell scripts",
+  "help_page: tests that render help pages",
   "pulp_ostree: pulp_ostree tests",
 ]
 
@@ -29,8 +30,6 @@ explicit_package_bases = true
 
 [[tool.mypy.overrides]]
 module = [
-  "urllib3.*",
-  "pygments.*",
   "click_shell.*",
   "schema.*",
 ]

tests/scripts/pulp_ostree/test_modify_content.sh
@@ -3,6 +3,8 @@
 # shellcheck source=tests/scripts/config.source
 . "$(dirname "$(dirname "$(realpath "$0")")")/config.source"
 
+CLI15="$(python -c "from packaging.version import parse; from pulpcore.cli.common import __version__; print(parse(__version__) >= parse('0.15.0.dev'))")"
+
 cleanup() {
   pulp ostree repository destroy --name "cli_test_ostree_repository1" || true
   pulp ostree repository destroy --name "cli_test_ostree_repository2" || true
@@ -16,15 +18,27 @@ expect_succ pulp ostree repository create --name "cli_test_ostree_repository1"
 expect_succ pulp ostree repository sync --name "cli_test_ostree_repository1" \
   --remote "cli_test_ostree_remote"
 
-expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
-  --type "ref"
+if [ "$CLI15" = "True" ]
+then
+  expect_succ pulp ostree repository content --type "ref" list \
+    --repository "cli_test_ostree_repository1"
+else
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+    --type "ref"
+fi
 
 COMMIT_CHECKSUM1=$(echo "$OUTPUT" | jq -r ".[0].checksum")
 COMMIT_CHECKSUM2=$(echo "$OUTPUT" | jq -r ".[1].checksum")
 REF_NAME2=$(echo "$OUTPUT" | jq -r ".[1].name")
 
-expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
-  --type "config"
+if [ "$CLI15" = "True" ]
+then
+  expect_succ pulp ostree repository content --type "config" list \
+    --repository "cli_test_ostree_repository1"
+else
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+    --type "config"
+fi
 
 CONFIG_HREF=$(echo "$OUTPUT" | jq -r ".[0].pulp_href")
 
@@ -64,20 +78,49 @@ expect_succ pulp ostree repository ref add --repository "cli_test_ostree_reposit
 
 # list content stored within the latest repository versions
 expect_succ pulp ostree repository ref list --repository "cli_test_ostree_repository1"
-expect_succ pulp ostree repository ref list --repository "cli_test_ostree_repository1" \
-  --type "ref"
+
+if [ "$CLI15" = "True" ]
+then
+  expect_succ pulp ostree repository ref --type "ref" list \
+    --repository "cli_test_ostree_repository1"
+else
+  expect_succ pulp ostree repository ref list --repository "cli_test_ostree_repository1" \
+    --type "ref"
+fi
 
 expect_succ pulp ostree repository commit list --repository "cli_test_ostree_repository1"
-expect_succ pulp ostree repository commit list --repository "cli_test_ostree_repository1" \
-  --type "commit"
+
+if [ "$CLI15" = "True" ]
+then
+  expect_succ pulp ostree repository commit --type "commit" list \
+    --repository "cli_test_ostree_repository1"
+else
+  expect_succ pulp ostree repository commit list --repository "cli_test_ostree_repository1" \
+    --type "commit"
+fi
 
 expect_succ pulp ostree repository config list --repository "cli_test_ostree_repository1"
-expect_succ pulp ostree repository config list --repository "cli_test_ostree_repository1" \
-  --type "config"
+
+if [ "$CLI15" = "True" ]
+then
+  expect_succ pulp ostree repository config --type "config" list \
+    --repository "cli_test_ostree_repository1"
+else
+  expect_succ pulp ostree repository config list --repository "cli_test_ostree_repository1" \
+    --type "config"
+fi
 
 expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1"
 
-expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
-  --type "all"
-expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
-  --type "commit"
+if [ "$CLI15" = "True" ]
+then
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+    --all-types
+  expect_succ pulp ostree repository content --type "commit" list \
+    --repository "cli_test_ostree_repository1"
+else
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+    --type "all"
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+    --type "commit"
+fi

tests/test_help_pages.py
@@ -5,7 +5,13 @@ from unittest.mock import PropertyMock, patch
 import click
 import pytest
 from click.testing import CliRunner
-from pulpcore.cli.common import main
+
+try:
+    from pulp_cli import load_plugins
+
+    main = load_plugins()
+except ImportError:
+    from pulpcore.cli.common import main
 
 
 def traverse_commands(command, args):
@@ -15,14 +21,15 @@ def traverse_commands(command, args):
             yield from traverse_commands(sub, args + [name])
 
 
+@pytest.mark.help_page
 @pytest.mark.parametrize(
     "args", traverse_commands(main.commands["ostree"], ["ostree"]), ids=" ".join
 )
-@patch("pulpcore.cli.common.PulpContext.api", new_callable=PropertyMock)
+@patch("pulpcore.cli.common.context.PulpContext.api", new_callable=PropertyMock)
 def test_access_help(_api, args):
     """Test, that all help screens are accessible without touching the api property."""
     runner = CliRunner()
     result = runner.invoke(main, args + ["--help"])
     assert result.exit_code == 0
-    assert result.stdout.startswith("Usage:")
+    assert result.stdout.startswith("Usage:") or result.stdout.startswith("DeprecationWarning:")
     _api.assert_not_called()