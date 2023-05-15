95cc2dd881785194ab77f6914853cc3d25fd3dee
Lubos Mjachky <lmjachky@redhat.com>
2023-05-15 16:06:25 +0200

    Bump the patch dev version

--- pulpcore/cli/ostree/__init__.py
+++ pulpcore/cli/ostree/__init__.py
@@ -7,7 +7,7 @@ from pulpcore.cli.ostree.distribution import distribution
 from pulpcore.cli.ostree.remote import remote
 from pulpcore.cli.ostree.repository import repository
 
-__version__ = "0.1.0"
+__version__ = "0.1.1.dev"
 
 
 @pulp_group("ostree")
