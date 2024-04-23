40170e46a8d107d894075eb4ce553acba5920f7a
pulpbot <pulp-infra@redhat.com>
2024-04-23 17:30:52 +0000

    Bump version to 0.4.0.dev
    [noissue]

--- pulpcore/cli/ostree/__init__.py
+++ pulpcore/cli/ostree/__init__.py
@@ -7,7 +7,7 @@ from pulpcore.cli.ostree.distribution import distribution
 from pulpcore.cli.ostree.remote import remote
 from pulpcore.cli.ostree.repository import repository
 
-__version__ = "0.3.0.dev"
+__version__ = "0.4.0.dev"
 
 
 @pulp_group("ostree")
