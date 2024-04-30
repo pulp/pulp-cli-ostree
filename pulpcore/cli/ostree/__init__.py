72b49aea358de6db90c27105b994ecded50cc092
pulpbot <pulp-infra@redhat.com>
2024-04-30 12:45:08 +0000

    Bump version to 0.5.0.dev
    [noissue]

--- pulpcore/cli/ostree/__init__.py
+++ pulpcore/cli/ostree/__init__.py
@@ -11,7 +11,7 @@ from pulpcore.cli.ostree.repository import repository
 translation = get_translation(__package__)
 _ = translation.gettext
 
-__version__ = "0.4.0.dev"
+__version__ = "0.5.0.dev"
 
 
 @pulp_group("ostree")
