45c078901e674d592035b6c62d8131d661b2ac24
Jitka Halova <jobselko@redhat.com>
2026-08-17 13:03:56 +0200

    Fix parallel CLI test name collisions
    Assisted By: Claude Opus 4.6

--- tests/scripts/pulp_ostree/test_importing_commits_with_import_commits.sh
+++ tests/scripts/pulp_ostree/test_importing_commits_with_import_commits.sh
@@ -5,8 +5,8 @@ set -eu
 . "$(dirname "$(dirname "$(realpath "$0")")")/config.source"
 
 cleanup() {
-  pulp ostree repository destroy --name "cli_test_ostree_repository" || true
-  pulp ostree distribution destroy --name "cli_test_ostree_distro" || true
+  pulp ostree repository destroy --name "cli_test_ostree_import_commits_repository" || true
+  pulp ostree distribution destroy --name "cli_test_ostree_import_commits_distro" || true
   pulp orphan cleanup || true
 }
 trap cleanup EXIT
@@ -14,15 +14,15 @@ trap cleanup EXIT
 wget --no-parent -r "$OSTREE_REMOTE_URL"
 tar --exclude="index.html" -cvf "fixtures_small_repo.tar" -C "$OSTREE_DOWNLOADED_REPO_PATH" "small"
 
-expect_succ pulp ostree repository create --name "cli_test_ostree_repository"
+expect_succ pulp ostree repository create --name "cli_test_ostree_import_commits_repository"
 
 if pulp debug has-plugin --name "ostree" --specifier ">=2.0.0"
 then
-  expect_succ pulp ostree repository import-all --name "cli_test_ostree_repository" \
+  expect_succ pulp ostree repository import-all --name "cli_test_ostree_import_commits_repository" \
     --file "fixtures_small_repo.tar" \
     --repository_name "small"
 else
-  expect_succ pulp ostree repository import-commits --name "cli_test_ostree_repository" \
+  expect_succ pulp ostree repository import-commits --name "cli_test_ostree_import_commits_repository" \
     --file "fixtures_small_repo.tar" \
     --repository_name "small"
 fi
@@ -42,18 +42,18 @@ tar -cvf "fixtures_small_repo_new_commit.tar" "small"
 
 if pulp debug has-plugin --name "ostree" --specifier ">=2.0.0"
 then
-  expect_succ pulp ostree repository import-commits --name "cli_test_ostree_repository" \
+  expect_succ pulp ostree repository import-commits --name "cli_test_ostree_import_commits_repository" \
     --file "fixtures_small_repo_new_commit.tar" \
     --repository_name "small" \
     --ref "stable"
 else
-  expect_succ pulp ostree repository import-commits --name "cli_test_ostree_repository" \
+  expect_succ pulp ostree repository import-commits --name "cli_test_ostree_import_commits_repository" \
     --file "fixtures_small_repo_new_commit.tar" \
     --repository_name "small" \
     --ref "stable" \
     --parent_commit "$LATEST_COMMIT"
 fi
 
-expect_succ pulp ostree distribution create --name "cli_test_ostree_distro" \
-  --base-path "cli_test_ostree_distro" \
-  --repository "cli_test_ostree_repository"
+expect_succ pulp ostree distribution create --name "cli_test_ostree_import_commits_distro" \
+  --base-path "cli_test_ostree_import_commits_distro" \
+  --repository "cli_test_ostree_import_commits_repository"
--- tests/scripts/pulp_ostree/test_modifying_content.sh
+++ tests/scripts/pulp_ostree/test_modifying_content.sh
@@ -7,24 +7,24 @@ set -eu
 CLI15="$(python -c "from packaging.version import parse; from pulpcore.cli.common import __version__; print(parse(__version__) >= parse('0.15.0.dev'))")"
 
 cleanup() {
-  pulp ostree repository destroy --name "cli_test_ostree_repository1" || true
-  pulp ostree repository destroy --name "cli_test_ostree_repository2" || true
-  pulp ostree remote destroy --name "cli_test_ostree_remote" || true
+  pulp ostree repository destroy --name "cli_test_ostree_modify_repository1" || true
+  pulp ostree repository destroy --name "cli_test_ostree_modify_repository2" || true
+  pulp ostree remote destroy --name "cli_test_ostree_modify_remote" || true
   pulp orphan cleanup || true
 }
 trap cleanup EXIT
 
-expect_succ pulp ostree remote create --name "cli_test_ostree_remote" --url "$OSTREE_REMOTE_URL"
-expect_succ pulp ostree repository create --name "cli_test_ostree_repository1"
-expect_succ pulp ostree repository sync --name "cli_test_ostree_repository1" \
-  --remote "cli_test_ostree_remote"
+expect_succ pulp ostree remote create --name "cli_test_ostree_modify_remote" --url "$OSTREE_REMOTE_URL"
+expect_succ pulp ostree repository create --name "cli_test_ostree_modify_repository1"
+expect_succ pulp ostree repository sync --name "cli_test_ostree_modify_repository1" \
+  --remote "cli_test_ostree_modify_remote"
 
 if [ "$CLI15" = "True" ]
 then
   expect_succ pulp ostree repository content --type "ref" list \
-    --repository "cli_test_ostree_repository1"
+    --repository "cli_test_ostree_modify_repository1"
 else
-  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_modify_repository1" \
     --type "ref"
 fi
 
@@ -35,93 +35,93 @@ REF_NAME2=$(echo "$OUTPUT" | jq -r ".[1].name")
 if [ "$CLI15" = "True" ]
 then
   expect_succ pulp ostree repository content --type "config" list \
-    --repository "cli_test_ostree_repository1"
+    --repository "cli_test_ostree_modify_repository1"
 else
-  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_modify_repository1" \
     --type "config"
 fi
 
 CONFIG_HREF=$(echo "$OUTPUT" | jq -r ".[0].pulp_href")
 
-expect_succ pulp ostree repository create --name "cli_test_ostree_repository2"
+expect_succ pulp ostree repository create --name "cli_test_ostree_modify_repository2"
 
 # add content to the second repository
-expect_succ pulp ostree repository commit add --repository "cli_test_ostree_repository2" \
+expect_succ pulp ostree repository commit add --repository "cli_test_ostree_modify_repository2" \
   --checksum "$COMMIT_CHECKSUM1"
-expect_succ pulp ostree repository ref add --repository "cli_test_ostree_repository2" \
+expect_succ pulp ostree repository ref add --repository "cli_test_ostree_modify_repository2" \
   --name "$REF_NAME2" \
   --checksum "$COMMIT_CHECKSUM2"
-expect_succ pulp ostree repository config add --repository "cli_test_ostree_repository2" \
+expect_succ pulp ostree repository config add --repository "cli_test_ostree_modify_repository2" \
   --pulp_href "$CONFIG_HREF"
 
 # remove the added content from the second repository
-expect_succ pulp ostree repository commit remove --repository "cli_test_ostree_repository2" \
+expect_succ pulp ostree repository commit remove --repository "cli_test_ostree_modify_repository2" \
   --checksum "$COMMIT_CHECKSUM1"
-expect_succ pulp ostree repository ref remove --repository "cli_test_ostree_repository2" \
+expect_succ pulp ostree repository ref remove --repository "cli_test_ostree_modify_repository2" \
   --name "$REF_NAME2" \
   --checksum "$COMMIT_CHECKSUM2"
-expect_succ pulp ostree repository config remove --repository "cli_test_ostree_repository2" \
+expect_succ pulp ostree repository config remove --repository "cli_test_ostree_modify_repository2" \
   --pulp_href "$CONFIG_HREF"
 
 # remove the original content from the first repository
-expect_succ pulp ostree repository commit remove --repository "cli_test_ostree_repository1" \
+expect_succ pulp ostree repository commit remove --repository "cli_test_ostree_modify_repository1" \
   --checksum "$COMMIT_CHECKSUM1"
-expect_succ pulp ostree repository ref remove --repository "cli_test_ostree_repository1" \
+expect_succ pulp ostree repository ref remove --repository "cli_test_ostree_modify_repository1" \
   --name "$REF_NAME2" \
   --checksum "$COMMIT_CHECKSUM2"
 
 # add the content back to the first repository
-expect_succ pulp ostree repository commit add --repository "cli_test_ostree_repository1" \
+expect_succ pulp ostree repository commit add --repository "cli_test_ostree_modify_repository1" \
   --checksum "$COMMIT_CHECKSUM1"
-expect_succ pulp ostree repository ref add --repository "cli_test_ostree_repository1" \
+expect_succ pulp ostree repository ref add --repository "cli_test_ostree_modify_repository1" \
   --name "$REF_NAME2" \
   --checksum "$COMMIT_CHECKSUM2"
 
 # list content stored within the latest repository versions
-expect_succ pulp ostree repository ref list --repository "cli_test_ostree_repository1"
+expect_succ pulp ostree repository ref list --repository "cli_test_ostree_modify_repository1"
 
 if [ "$CLI15" = "True" ]
 then
   expect_succ pulp ostree repository ref --type "ref" list \
-    --repository "cli_test_ostree_repository1"
+    --repository "cli_test_ostree_modify_repository1"
 else
-  expect_succ pulp ostree repository ref list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository ref list --repository "cli_test_ostree_modify_repository1" \
     --type "ref"
 fi
 
-expect_succ pulp ostree repository commit list --repository "cli_test_ostree_repository1"
+expect_succ pulp ostree repository commit list --repository "cli_test_ostree_modify_repository1"
 
 if [ "$CLI15" = "True" ]
 then
   expect_succ pulp ostree repository commit --type "commit" list \
-    --repository "cli_test_ostree_repository1"
+    --repository "cli_test_ostree_modify_repository1"
 else
-  expect_succ pulp ostree repository commit list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository commit list --repository "cli_test_ostree_modify_repository1" \
     --type "commit"
 fi
 
-expect_succ pulp ostree repository config list --repository "cli_test_ostree_repository1"
+expect_succ pulp ostree repository config list --repository "cli_test_ostree_modify_repository1"
 
 if [ "$CLI15" = "True" ]
 then
   expect_succ pulp ostree repository config --type "config" list \
-    --repository "cli_test_ostree_repository1"
+    --repository "cli_test_ostree_modify_repository1"
 else
-  expect_succ pulp ostree repository config list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository config list --repository "cli_test_ostree_modify_repository1" \
     --type "config"
 fi
 
-expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1"
+expect_succ pulp ostree repository content list --repository "cli_test_ostree_modify_repository1"
 
 if [ "$CLI15" = "True" ]
 then
-  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_modify_repository1" \
     --all-types
   expect_succ pulp ostree repository content --type "commit" list \
-    --repository "cli_test_ostree_repository1"
+    --repository "cli_test_ostree_modify_repository1"
 else
-  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_modify_repository1" \
     --type "all"
-  expect_succ pulp ostree repository content list --repository "cli_test_ostree_repository1" \
+  expect_succ pulp ostree repository content list --repository "cli_test_ostree_modify_repository1" \
     --type "commit"
 fi
